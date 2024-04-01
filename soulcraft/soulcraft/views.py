637fe11f1970441f46f8f2e96ee8d040457fc217
paul futher <37282422+Paulfuther@users.noreply.github.com>
2024-04-01 14:43:19 -0400

    Upgrades to error messagin

--- soulcraft/soulcraft/views.py
+++ soulcraft/soulcraft/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 
 def error_403(request, exception):
     data = {}
-    return render(request, 'incident/403.html', data)
+    return render(request, "incident/403.html", data)
 
 
 def error_500(request, exception=None):
     data = {}
-    return render(request, 'incident/500.html', data)
+    return render(request, "incident/500.html", data)
+
+
+def error_404(request, exception=None):
+    data = {}
+    return render(request, "incident/404.html", data)
